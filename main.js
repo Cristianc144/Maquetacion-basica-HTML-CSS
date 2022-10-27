3ec77450e230425fb2c168bfb158a7bb0f2f549c
Cristian Caicedo <cfcaicedoca@unal.edu.co>
2022-10-27 17:44:58 -0500

    Se realizó la primera adaptación de la web en telefonos moviles

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navbarEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 navbarEmail.addEventListener('click', toggleDesktopMenu)
+menu.addEventListener('click', toggleMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMenu() {
+    mobileMenu.classList.toggle('inactive-mobile')
 }
