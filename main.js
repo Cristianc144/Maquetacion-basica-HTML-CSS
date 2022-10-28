17e5af7f0d1d6a7ce915a0b998645832f48d981a
Cristian Caicedo <cfcaicedoca@unal.edu.co>
2022-10-28 01:09:12 -0500

    Se realizó cambios en la configuración del carrito, tanto en plataformas web, como en telefonos móviles y se empezó a trabajar con la lista de productos, con el fin de que la página web quede totalmente dinámica

--- main.js
+++ main.js
@@ -2,14 +2,41 @@ const navbarEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menu = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarrito = document.querySelector('.navbar-shopping-cart')
+const productDetail = document.querySelector('.product-detail')
+const arrow = document.getElementById('arrow')
 
-navbarEmail.addEventListener('click', toggleDesktopMenu)
-menu.addEventListener('click', toggleMenu)
+const productList = []
+
+escuchadorEventosToggle(navbarEmail, toggleDesktopMenu)
+escuchadorEventosToggle(menu, toggleMenu)
+escuchadorEventosToggle(menuCarrito, toggleCarrito)
+escuchadorEventosToggle(arrow, toggleCarrito)
+
+function escuchadorEventosToggle(evento, funcion) {
+    evento.addEventListener('click', funcion)
+}
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+
+    if (!productDetail.classList.contains('inactive-detail') && !desktopMenu.classList.contains('inactive')) {
+        productDetail.classList.toggle('inactive-detail')
+    }
 }
 
 function toggleMenu() {
     mobileMenu.classList.toggle('inactive-mobile')
+
+    if (!mobileMenu.classList.contains('inactive-mobile') && !productDetail.classList.contains('inactive-detail')) {
+        productDetail.classList.toggle('inactive-detail')
+    }
+}
+
+function toggleCarrito() {
+    productDetail.classList.toggle('inactive-detail')
+
+    if (!mobileMenu.classList.contains('inactive-mobile') && !productDetail.classList.contains('inactive-detail')) {
+        mobileMenu.classList.toggle('inactive-mobile')
+    }
 }
